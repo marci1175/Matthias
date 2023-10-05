6bd385d67f4e42b824513b6875e9ed271225cea0
Marcell Varga <v.marcell4000@gmail.com>
2023-10-05 21:50:30 +0200

    yes

--- src/app.rs
+++ src/app.rs
@@ -1,4 +1,4 @@
-use egui::{Layout, Align, Image};
+use egui::{Layout, Align, Image, vec2, Vec2};
 
 mod client;
 
@@ -6,14 +6,24 @@ mod client;
 #[derive(serde::Deserialize, serde::Serialize)]
 #[serde(default)] 
 pub struct TemplateApp {
+    //window options
+    #[serde(skip)]
+    window_size: Vec2,
+
+
     #[serde(skip)]
     client_mode: bool,
+    #[serde(skip)]
+    server_mode: bool,
 }
 
 impl Default for TemplateApp {
     fn default() -> Self {
         Self {
+            //window options
+            window_size: vec2(700., 300.),
             client_mode: false,
+            server_mode: false,
         }
     }
 }
@@ -38,44 +48,68 @@ impl eframe::App for TemplateApp {
 
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         /*devlog:
-        TODO: RESIZE IMAGES, MAKE QUALITY BETTER
+        TODO: MAKE QUALITY BETTER!!!
         
         
         */
-    
+
+        //window options
+        _frame.set_window_size(self.window_size);
 
 
         //for image loading
         egui_extras::install_image_loaders(ctx);
 
-        egui::CentralPanel::default().show(ctx, |ui|{
-            let Layout = Layout::left_to_right(Align::Center);
-            
-            ui.columns(2, |ui|{
-
-                ui[0].with_layout(Layout::centered_and_justified(egui::Direction::TopDown), |ui|{
-                    ui.add(
-                        egui::widgets::ImageButton::new(
-                            egui::include_image!("../icons/client.png")
-                        )
-                    );
-                });
+        //Main page
+        if !(self.client_mode || self.server_mode)
+        {
+            egui::CentralPanel::default().show(ctx, |ui|{
+                let Layout = Layout::left_to_right(Align::Center);
                 
-
-                ui[1].with_layout(Layout::centered_and_justified(egui::Direction::TopDown), |ui|{
-                    ui.add(egui::widgets::ImageButton::new(
-                        egui::include_image!("../icons/server.png")
-
-                        )
-                    );
+                ui.columns(2, |ui|{
+    
+                    ui[0].with_layout(Layout::centered_and_justified(egui::Direction::TopDown), |ui|{
+                        if ui.add(
+                            egui::widgets::ImageButton::new(
+                                egui::include_image!("../icons/client.png")
+                            )
+                        ).on_hover_text("Enter Client mode")
+                         .clicked() {
+                            self.client_mode = true;
+                         };
+                    });
                     
-                    
-                });     
-
-            });
-
+    
+                    ui[1].with_layout(Layout::centered_and_justified(egui::Direction::TopDown), |ui|{
+                        if ui.add(egui::widgets::ImageButton::new(
+                            egui::include_image!("../icons/server.png")
+    
+                            )
+                        ).on_hover_text("Enter Server mode")
+                         .clicked() {
+                            self.server_mode = true;
+                         };
                         
-        });
+                        
+                    });     
+    
+                });
+    
+                            
+            });
+        }
+        
+        //Server page
+        if self.server_mode
+        {
+
+        }
+
+        //Client page
+        if self.client_mode
+        {
+            
+        }
 
     }
 }
