017255532b3086235cbd00b056ad88556294f8be
Varga Marcell <128537619+marci1175@users.noreply.github.com>
2024-03-28 11:23:50 +0100

    optimisation

--- src/app/ui/client_ui/message_instances/audio.rs
+++ src/app/ui/client_ui/message_instances/audio.rs
@@ -35,176 +35,157 @@ impl TemplateApp {
                 audio.index
             ));
             ui.allocate_ui(vec2(300., 150.), |ui| {
-                match path.exists() {
-                    true => {
-                        //if we already have the sound file :::
+                ui.with_layout(Layout::top_down(Align::Center), |ui| {
+                    match self.client_ui.audio_playback.sink_list[current_index_in_message_list]
+                        .as_mut()
+                    {
+                        Some(sink) => match sink.is_paused() {
+                            true => {
+                                if ui.button("Play").clicked() {
+                                    sink.play();
+                                };
+                            }
+                            false => {
+                                //ui.label(format!("{:?}", self.client_ui.audio_playback.settings_list[current_index_in_message_list].cursor.cursor.lock().unwrap().remaining_slice().len()));
+                                //let cursor = self.client_ui.audio_playback.settings_list[current_index_in_message_list].cursor.cursor.lock().unwrap();
+                                if ui.button("Stop").clicked() {
+                                    sink.clear();
+                                    //Reset value
+                                    self.client_ui.audio_playback.sink_list[current_index_in_message_list] = None;
+                                }
+                            }
+                        },
+                        None => {
+                            if ui.button("Play").clicked() {
+                                self.client_ui.send_on_ip_base64_encoded =
+                                    general_purpose::URL_SAFE_NO_PAD.encode(self.client_ui.send_on_ip.clone());
+                                let file = PathBuf::from(format!(
+                                    "{}\\Matthias\\Client\\{}\\Audios\\{}",
+                                    env!("APPDATA"),
+                                    self.client_ui.send_on_ip_base64_encoded,
+                                    audio.index
+                                ));
 
-                        ui.with_layout(Layout::top_down(Align::Center), |ui| {
-                            match self.client_ui.audio_playback.sink_list[current_index_in_message_list]
-                                .as_mut()
-                            {
-                                Some(sink) => match sink.is_paused() {
-                                    true => {
-                                        if ui.button("Play").clicked() {
-                                            sink.play();
-                                        };
-                                    }
-                                    false => {
-                                        //ui.label(format!("{:?}", self.client_ui.audio_playback.settings_list[current_index_in_message_list].cursor.cursor.lock().unwrap().remaining_slice().len()));
-                                        //let cursor = self.client_ui.audio_playback.settings_list[current_index_in_message_list].cursor.cursor.lock().unwrap();
-                                        if ui.button("Stop").clicked() {
-                                            sink.clear();
-                                            //Reset value
-                                            self.client_ui.audio_playback.sink_list[current_index_in_message_list] = None;
-                                        }
-                                    }
-                                },
-                                None => {
-                                    if ui.button("Play").clicked() {
-                                        self.client_ui.send_on_ip_base64_encoded =
-                                            general_purpose::URL_SAFE_NO_PAD.encode(self.client_ui.send_on_ip.clone());
-                                        let file = PathBuf::from(format!(
-                                            "{}\\Matthias\\Client\\{}\\Audios\\{}",
-                                            env!("APPDATA"),
-                                            self.client_ui.send_on_ip_base64_encoded,
-                                            audio.index
-                                        ));
+                                //If the user has clicked the play button only then we download the desirted audio file! Great optimisation
+                                if !file.exists() {
+                                    let sender = self.audio_save_tx.clone();
 
-                                        let file_stream_to_be_read = fs::read(file).unwrap_or_default();
-                                        self.client_ui.audio_playback.settings_list[current_index_in_message_list].cursor = PlaybackCursor::new(file_stream_to_be_read);
-                                        self.client_ui.audio_playback.sink_list
-                                            [current_index_in_message_list] = Some(
-                                            Sink::try_new(&self.client_ui.audio_playback.stream_handle)
-                                                .unwrap(),
-                                        );
-                                        let sink = self.client_ui.audio_playback.sink_list
-                                            [current_index_in_message_list]
-                                            .as_mut()
-                                            .unwrap();
-
-                                        let source = Decoder::new(self.client_ui.audio_playback.settings_list[current_index_in_message_list].cursor.clone() /*We can assume its always Some because we just set it to some above (lol)*/);
-                                        match source {
-                                            Ok(source) => {
-
-                                                sink.append(source);
-                                                sink.play();
-
+                                    let message = ClientMessage::construct_audio_request_msg(
+                                        audio.index,
+                                        self.client_ui.client_password.clone(),
+                                        self.login_username.clone(),
+                                    );
+            
+                                    let connection = self.client_connection.clone();
+            
+                                    tokio::spawn(async move {
+                                        match client::send_msg(connection, message).await {
+                                            Ok(ok) => {
+                                                match sender.send(ok) {
+                                                    Ok(_) => {}
+                                                    Err(err) => {
+                                                        println!("{}", err);
+                                                    }
+                                                };
                                             }
                                             Err(err) => {
-                                                dbg!(err);
+                                                println!("{err} ln 264")
                                             }
                                         }
-
-                                    };
+                                    });
                                 }
-                            }
-                        });
 
-                        //Set properties of audio stream, when there is one
-                        if let Some(sink) =
-                            self.client_ui.audio_playback.sink_list[current_index_in_message_list].as_mut()
-                        {
-                            //Set volume
-                            sink.set_volume(
-                                self.client_ui.audio_playback.settings_list[current_index_in_message_list]
-                                    .volume,
-                            );
-
-                            sink.set_speed(
-                                self.client_ui.audio_playback.settings_list[current_index_in_message_list]
-                                    .speed,
-                            );
-                        }
-                        /*
-                        let pos = self.client_ui.audio_playback.settings_list[current_index_in_message_list].cursor_offset;
-                        if let Some(cursor) = self.client_ui.audio_playback.settings_list[current_index_in_message_list].cursor.as_mut() {
-                            cursor.set_position(pos);
-                            let range = self.client_ui.audio_playback.settings_list
-                            [current_index_in_message_list]
-                            .cursor.clone().unwrap().position() + self.client_ui.audio_playback.settings_list
-                            [current_index_in_message_list]
-                            .cursor.clone().unwrap().remaining_slice().len() as u64;
-                            //Cursor
-                            ui.add(
-                                egui::Slider::new(
-                                    &mut self.client_ui.audio_playback.settings_list
-                                        [current_index_in_message_list]
-                                        .cursor_offset,
-                                    0..=range,
-                                )
-                                .text("Volume")
-                                .step_by(1.)
-                            );
-                        }
-                        */
-
-                        ui.label(&audio.file_name);
-                        //Audio volume
-                        ui.add(
-                            egui::Slider::new(
-                                &mut self.client_ui.audio_playback.settings_list
+                                let file_stream_to_be_read = fs::read(file).unwrap_or_default();
+                                self.client_ui.audio_playback.settings_list[current_index_in_message_list].cursor = PlaybackCursor::new(file_stream_to_be_read);
+                                self.client_ui.audio_playback.sink_list
+                                    [current_index_in_message_list] = Some(
+                                    Sink::try_new(&self.client_ui.audio_playback.stream_handle)
+                                        .unwrap(),
+                                );
+                                let sink = self.client_ui.audio_playback.sink_list
                                     [current_index_in_message_list]
-                                    .volume,
-                                0.0..=5.,
-                            )
-                            .text("Volume")
-                            .step_by(0.05),
-                        );
-                        //Audio speed
-                        ui.add(
-                            egui::Slider::new(
-                                &mut self.client_ui.audio_playback.settings_list
-                                    [current_index_in_message_list]
-                                    .speed,
-                                0.1..=5.,
-                            )
-                            .text("Speed")
-                            .step_by(0.05),
-                        );
-                    }
-                    false => {
-                        //create decoy file, to manually create a race condition
-                        if let Err(err) = std::fs::write(
-                            path,
-                            "This is a placeholder file, this will get overwritten (hopefully)",
-                        ) {
-                            println!("Error when creating a decoy: {err}");
-                            return;
-                        };
+                                    .as_mut()
+                                    .unwrap();
 
-                        //check if we are visible
-                        if !ui.is_visible() {
-                            return;
+                                let source = Decoder::new(self.client_ui.audio_playback.settings_list[current_index_in_message_list].cursor.clone() /*We can assume its always Some because we just set it to some above (lol)*/);
+                                match source {
+                                    Ok(source) => {
+
+                                        sink.append(source);
+                                        sink.play();
+
+                                    }
+                                    Err(err) => {
+                                        dbg!(err);
+                                    }
+                                }
+
+                            };
                         }
-
-                        //We dont have file on our local system so we have to ask the server to provide it
-                        let sender = self.audio_save_tx.clone();
-
-                        let message = ClientMessage::construct_audio_request_msg(
-                            audio.index,
-                            self.client_ui.client_password.clone(),
-                            self.login_username.clone(),
-                        );
-
-                        let connection = self.client_connection.clone();
-
-                        tokio::spawn(async move {
-                            match client::send_msg(connection, message).await {
-                                Ok(ok) => {
-                                    match sender.send(ok) {
-                                        Ok(_) => {}
-                                        Err(err) => {
-                                            println!("{}", err);
-                                        }
-                                    };
-                                }
-                                Err(err) => {
-                                    println!("{err} ln 264")
-                                }
-                            }
-                        });
                     }
-                };
+                });
+
+                //Set properties of audio stream, when there is one
+                if let Some(sink) =
+                    self.client_ui.audio_playback.sink_list[current_index_in_message_list].as_mut()
+                {
+                    //Set volume
+                    sink.set_volume(
+                        self.client_ui.audio_playback.settings_list[current_index_in_message_list]
+                            .volume,
+                    );
+
+                    sink.set_speed(
+                        self.client_ui.audio_playback.settings_list[current_index_in_message_list]
+                            .speed,
+                    );
+                }
+                /*
+                let pos = self.client_ui.audio_playback.settings_list[current_index_in_message_list].cursor_offset;
+                if let Some(cursor) = self.client_ui.audio_playback.settings_list[current_index_in_message_list].cursor.as_mut() {
+                    cursor.set_position(pos);
+                    let range = self.client_ui.audio_playback.settings_list
+                    [current_index_in_message_list]
+                    .cursor.clone().unwrap().position() + self.client_ui.audio_playback.settings_list
+                    [current_index_in_message_list]
+                    .cursor.clone().unwrap().remaining_slice().len() as u64;
+                    //Cursor
+                    ui.add(
+                        egui::Slider::new(
+                            &mut self.client_ui.audio_playback.settings_list
+                                [current_index_in_message_list]
+                                .cursor_offset,
+                            0..=range,
+                        )
+                        .text("Volume")
+                        .step_by(1.)
+                    );
+                }
+                */
+
+                ui.label(&audio.file_name);
+                //Audio volume
+                ui.add(
+                    egui::Slider::new(
+                        &mut self.client_ui.audio_playback.settings_list
+                            [current_index_in_message_list]
+                            .volume,
+                        0.0..=5.,
+                    )
+                    .text("Volume")
+                    .step_by(0.05),
+                );
+                //Audio speed
+                ui.add(
+                    egui::Slider::new(
+                        &mut self.client_ui.audio_playback.settings_list
+                            [current_index_in_message_list]
+                            .speed,
+                        0.1..=5.,
+                    )
+                    .text("Speed")
+                    .step_by(0.05),
+                );
             });
         }
     }
