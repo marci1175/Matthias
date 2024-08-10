1b47a90e6862c9e6692bae0a9a8efd43eb5b2891
Varga Marcell <128537619+marci1175@users.noreply.github.com>
2024-08-11 00:23:51 +0200

    removed useless

--- check.cmd
+++
@@ -1,6 +0,0 @@
-cargo check --workspace 
-cargo check --workspace --all-features --lib 
-cargo fmt --all -- --check
-cargo clippy --workspace --all-features --  -D warnings -W clippy::all
-cargo test --workspace --all-features
-cargo test --workspace --doc
